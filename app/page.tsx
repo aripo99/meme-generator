c18b3acad9503fc1440f68ae74d6912e9c0d25a8
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-07 01:24:42 -0800

    Disable button if no file uploaded

--- app/page.tsx
+++ app/page.tsx
@@ -39,11 +39,13 @@ export default function Home() {
               <label className="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue-500 hover:text-white">
                 <FiUploadCloud className="w-8 h-8" />
                 <span className="mt-2 text-base leading-normal">
-                  Select a file
+                  {selectedFile ? 'Image Selected' : 'Select a file'}
                 </span>
                 <input type='file' className="hidden" onChange={handleFileChange} />
               </label>
-              <Button className="mt-6">Generate</Button>
+              <Button className={`mt-6 ${!selectedFile ? 'opacity-50 cursor-not-allowed' : 'hover:bg-blue-700'}`} disabled={!selectedFile}>
+                {selectedFile ? 'Ready to Generate' : 'Choose an Image'}
+              </Button>
             </div>
           </div>
         </form>
